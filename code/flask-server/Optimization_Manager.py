3ca737c01c15e3d98d92b0de9b7f48506a9daf61
Mattia <mattia.talacci@gmail.com>
2023-07-22 19:56:21 +0200

    add comment to Optimization_Manager

--- code/flask-server/Optimization_Manager.py
+++ code/flask-server/Optimization_Manager.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 from firebase_admin import db
 import pandas as pd
 from utils import *
@@ -5,42 +6,28 @@ from optimizer import *
 from datetime import datetime
 import time
 
-
-
-
-
-#FUNZIONA
+# Function to retrieve data of a specific problem session from the Firebase database
 def getDatabaseProblemData(sessionID):
     global status_list
     status_list.setProgress(sessionID, 'Gathering of data of Database Problem is running...')
+    
+    # Access the Firebase database and get the data for the given sessionID
     ref=db.reference('/')
     session_data = ref.child(sessionID).get()
 
+    # If no data is found, return None
     if not session_data:
         return None
 
+    # Extract unavailable dates from the session data using key 'dates'
     unavail_list = session_data.get('unavailList', {})
-    # Estrai le date dal dizionario utilizzando la chiave 'dates'
     dates_dict = unavail_list.get(sessionID, {}).get('dates', {})
-
-    # Converti le date da stringhe a oggetti datetime
+    # Conversion of dates from string to datetime object
     datetime_list = [datetime.fromisoformat(date_string) for date_string in dates_dict.values()]
 
-
+    # Extract and format relevant data for the problem session
     settings_data = session_data.get('settings', {})
     print('Settings data:\n',settings_data)
-    #print('exceptions: \n', settings_data['minDistanceCalls']['Exceptions'])
-    # min_distance_exams = settings_data.get('minDistanceExams')
-    # min_distance_calls_default = settings_data.get('minDistanceCalls', {}).get('Default')
-    # exceptions = settings_data['minDistanceCalls']['Exceptions']
-
-    # settings_list = [min_distance_exams, min_distance_calls_default]
-
-    # for key, value in exceptions.items():
-    #     settings_list.append([key, value])
-
-    # result_list = [[item.get('name'), item.get('type'), item.get('dates', [])] for item in unavail_list.values()]
-
     session_data = {
         "id": sessionID,
         "school": session_data.get('school'),
@@ -52,18 +39,18 @@ def getDatabaseProblemData(sessionID):
         "unavailList": datetime_list,
         "settings": settings_data #session_data.get('semester'),
     }
-
+    # Create a ProblemSession object using the extracted data
     problem_session = ProblemSession(**session_data)
 
     return problem_session
 
-#FUNZIONA
+# Function to retrieve exam data from the Excel file based on school and study course code
 def getDatabaseExam(cds_id, sessionID, school, percentage):
     global status_list
 
     try:
         status_list.setProgress(sessionID, percentage + ' Recupero dei dati del Database Esami in corso...')
-        # Leggi i dati dal foglio specificato del file Excel
+        # Read data from the specified sheet of the Excel file
         data = pd.read_excel('Database esami_'+school+'.xlsx', sheet_name=cds_id)
     except FileNotFoundError as e:
         status_list.setProgress(sessionID, percentage + ' File Excel non trovato: flask-server\Database esami_'+school+'.xlsx')
@@ -74,12 +61,11 @@ def getDatabaseExam(cds_id, sessionID, school, percentage):
         #print('Errore:', e)
         return 'sheet error'
 
-    # Creates an empty list for Exam objects
+    # Create an empty list to hold Exam objects
     resultsExams1 = []
 
-    # Iterate on dataframe rows
+    # Iterate over dataframe rows to create Exam objects and add them to the list
     for _, row in data.iterrows():
-        # Creates a dictionary for the data of the Exam object
         exam_data = {
             "cds": row['Cds'],
             "course_code": str(row['Course Code']),
@@ -101,19 +87,15 @@ def getDatabaseExam(cds_id, sessionID, school, percentage):
         exam1 = Exam(**exam_data)
         # Add the Exam object to the list
         resultsExams1.append(exam1)
-        #print(type(exam1.professor))
         count=0
-    # for ex in resultsExams1:
-    #     count+=1
-    #     print(f'{count}) {ex.toString()}')
         
     return resultsExams1
 
-#FUNZIONA
+# Function to create a list of optExam objects for optimization
 def createOptExamList(ExamList, sessionID, percentage):
     global status_list
     status_list.setProgress(sessionID, percentage + ' Exam list creation is running...')
-    # Creates an empty list for optExam objects
+    # Create an empty list to hold optExam objects
     resultsExams2 = []
 
     for exam in ExamList:
@@ -144,42 +126,38 @@ def createOptExamList(ExamList, sessionID, percentage):
         exam2 = optExam(**optexam_data)
         # Add the optExam object to the list
         resultsExams2.append(exam2)
-        #print(type(exam2.professor))
+
     return resultsExams2
 
-
+# Function to calculate effortWeight and timeWeight for each optExam object
 def createWeight(unprocessedExamList, semester, sessionID, percentage):
     global status_list
     status_list.setProgress(sessionID, percentage + ' Weight creation is running')
     # Creates weights for each optExam
     for exam in unprocessedExamList:
+        # Calculate effortWeight based on exam attributes
         exam.effortWeight = (exam.cfu * 3 + exam.passed_percentage * 4 + exam.average_mark * 4)/100
+         # Calculate timeWeight for each pair of exams in the list
         exam.timeWeight = [int(2**(-0.3 * abs(exam.sem - exam_j.sem)) * 1000) / 100 + 10 * ((exam.sem == semester) * (exam_j.sem == semester)) for exam_j in unprocessedExamList]
-        #exam.timeWeight = [int(2**(-0.3*abs(exam.sem - exam_j.exam.sem))*1000)/100 + 10*((exam.sem==semester).real)*((exam_j.exam.sem==semester).real) for exam_j in unprocessedExamList] 
+        
     return unprocessedExamList  
 
-
+# Function to add information about minimum distances to each optExam object
 def addDistances(unprocessedExamList, problem_session, sessionID, percentage):
     global status_list
     status_list.setProgress(sessionID, percentage + ' Distances adding is running...')
 
+    # Check for exceptions in the problem session settings
     if 'minDistanceCalls' in problem_session.settings and problem_session.settings['minDistanceCalls'].get('Exceptions') is not None:
         exceptions = problem_session.settings['minDistanceCalls']['Exceptions'].items()
     else:
         exceptions = []
-    
    
     for opt_exam in unprocessedExamList:
         opt_exam.minDistanceExams = problem_session.settings['minDistanceExams']
         exception_found=False
         for k, value in exceptions:
-            #print(f'addDistance: value: {value}')
-            
-            #print(f"addDistance: value.id type:{type(value['id'])}")
-            #print(f"addDistance: optExam.coursecode:{type(opt_exam.course_code)}")
-            #print(f"addDistance: optExam.coursecode:{opt_exam.course_code} == {value['id']}")
             if opt_exam.course_code == value['id']:
-                #print(opt_exam.course_code)
                 opt_exam.minDistanceCalls = int(value['distance'])
                 exception_found=True
         if(not exception_found):
@@ -187,50 +165,46 @@ def addDistances(unprocessedExamList, problem_session, sessionID, percentage):
 
     return unprocessedExamList
 
-#TESTARE IL MATCHING DI DATE
+# Function to merge unavailability data with each optExam object
 def addUnavailability(unprocessedExamList, problem_session, sessionID, percentage):
     global status_list
     status_list.setProgress(sessionID, percentage + ' Unavailability merging is running...')
     for opt_exam in unprocessedExamList:
         professor_list = []
-        professors = opt_exam.professor.split('-')  #Divides the string into hyphenated names
-        professor_list.extend(professors)  # Adds names to the professor_list
-        #print(professor_list)
+        professors = opt_exam.professor.split('-')  # Split professor names separated by hyphen
+        professor_list.extend(professors)           # Add names to the professor_list
 
         for k, value in problem_session.unavailList:
-            #print(f'{k} : {value}')
-
             if value['name'] in professor_list:
-                opt_exam.unavailDates.append(value['dates']) #qui carica una lista di liste, deve appendere solo gli elementi
-                # print('addUnavail:'+ str(opt_exam.unavailDates))
+                opt_exam.unavailDates.append(value['dates']) # Append unavailability dates of professors
             if value['type']==1:
-                opt_exam.unavailDates.append(value['dates'])
-        # print('addUnavail: assegnazione indisponibilità')
+                opt_exam.unavailDates.append(value['dates']) # Append other unavailability dates
+
     return unprocessedExamList
 
-
+# Function to manage the overall optimization process
 def runOptimizationManager(sessionID, callback):
     global status_list
     
     status_list.setProgress(sessionID, 'Optimization flow started')
-    # Get according to sessionID database values Problem
+    # Get problem session data from the database
     status_list.setProgress(sessionID, 'Gathering of data of Database Problem will start shortly')
     problem_session=getDatabaseProblemData(sessionID)
     status_list.setProgress(sessionID, 'Database Problem data gathering completed')
-    #print(problem_session)
-    ## Itera each school CdS
+    
+    # List of CdS for different schools
     if problem_session.school == 'Ing_Ind_Inf':
         cds_list = ['ATM','ELT']#, 'ELN', 'BIO', 'MTM', 'INF']
     if problem_session.school == 'Design':
-        cds_list = ['ATM']#,'ELT', 'ELN', 'BIO', 'MTM', 'INF']
+        cds_list = ['']
     if problem_session.school == 'AUIC':
-        cds_list = ['ATM']#,'ELT', 'ELN', 'BIO', 'MTM', 'INF']
+        cds_list = ['']
 
-        
     resultsExams = set()
     
+    # Iterate over each school CdS and perform optimization
     for index, cds_id in enumerate(cds_list, 1):
-        total_cds = len(cds_list)  # Numero totale di cds nella lista
+        total_cds = len(cds_list)  # Total number of CdS in the list
         percentage = f"Iterazione {index}/{total_cds}: {cds_id}"
         status_list.setProgress(sessionID, percentage + ' Gathering of data of Database Exam will start shortly')
         ExamList = getDatabaseExam(cds_id, sessionID, problem_session.school, percentage)
@@ -246,14 +220,11 @@ def runOptimizationManager(sessionID, callback):
         status_list.setProgress(sessionID, percentage + ' Exam list creation completed')
         unprocessedExamList=optExamList
        
+        # Update assignedDates of exams that are already scheduled
         for item_i in unprocessedExamList:
              for item_j in resultsExams:
                  if item_i.course_code == item_j.course_code:
                         item_i.assignedDates = item_j.assignedDates
-
-        # print(f'resultExams after {cds_id} check: ')
-        # for el in resultsExams:
-        #     print(el.toString())
         
         status_list.setProgress(sessionID, percentage + ' Weight creation will start shortly')
         unprocessedExamList1=createWeight(unprocessedExamList, problem_session.settings['currSemester'], sessionID, percentage)
@@ -263,7 +234,6 @@ def runOptimizationManager(sessionID, callback):
         unprocessedExamList2=addDistances(unprocessedExamList1, problem_session, sessionID, percentage)
         status_list.setProgress(sessionID, percentage + ' Distances adding completed')
         
-        
         status_list.setProgress(sessionID, percentage + ' Unavailability merging will start shortly')
         unprocessedExamList3=addUnavailability(unprocessedExamList2, problem_session, sessionID, percentage)
         status_list.setProgress(sessionID, percentage + ' Unavailability merging completed')
@@ -271,8 +241,7 @@ def runOptimizationManager(sessionID, callback):
         status_list.setProgress(sessionID, percentage + ' Optimization process will start shortly')
         [result, scheduledExam]=solveScheduling(unprocessedExamList3, problem_session)
 
-        ### HO INSERITO DEL TEMPO NULLO PER RIUSCIRE A FAR PARTIRE UN ALTRO SCHEDULING E OTTENERE LA RISPOSTA
-        time.sleep(5)
+        time.sleep(5)  # Time to allow starting another scheduling and obtaining the response
         if result == 1:
             status_list.setProgress(sessionID, percentage + ' Optimization process completed')
             status_list.setStatus(sessionID,'NOT SOLVED')
@@ -283,9 +252,9 @@ def runOptimizationManager(sessionID, callback):
                     pass
                 else:
                     resultsExams.add(element)
-                    # print(element.toString())
             status_list.setProgress(sessionID, percentage + ' Optimization process completed')
     if result== 0:        
         status_list.setStatus(sessionID,'SOLVED')
-        
+
+        # Call the callback function with the resultsExams and problem_session as arguments        
         callback(resultsExams, problem_session)
